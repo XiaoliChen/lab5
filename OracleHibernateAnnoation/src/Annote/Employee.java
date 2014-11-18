e39011c91d2120927672cd6d4622de80f3e510b8
Bert.Gibbons <bgibbons@udel.edu>
2014-11-18 16:10:21 -0500

    Initial Commit

---
+++ OracleHibernateAnnoation/src/Annote/Employee.java
@@ -0,0 +1,68 @@
+package Annote;
+
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMP")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_EMP")
+	@SequenceGenerator(name = "SEQ_EMP", sequenceName = "SEQ_EMP", allocationSize = 1)
+	@Column(name = "id")
+
+
+	private int id;
+
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+
+	@Column(name = "LAST_NAME")
+	private String lastName;
+
+	@Column(name = "SALARY")
+	private int salary;
+
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
